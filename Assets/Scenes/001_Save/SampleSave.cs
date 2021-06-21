b6f91384a02d45eabf4873b01bfcfdcb57d0245f
wanibananalab <wanideath@gmail.com>
2021-06-21 23:59:57 +0900

    セーブの実装

--- Assets/Scenes/001_Save/SampleSave.cs
+++ Assets/Scenes/001_Save/SampleSave.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Net;
 using UnityEngine;
 
 namespace Test
@@ -9,7 +11,13 @@ namespace Test
         // Start is called before the first frame update
         void Start()
         {
-        
+            var user = new User();
+            user.id = 333;
+            user.value = 100;
+            SaveSystem.Save(user);
+
+            user = SaveSystem.Load<User>();
+            Debug.Log(user.ToJson());
         }
 
         // Update is called once per frame
@@ -34,8 +42,34 @@ namespace Test
         }
     }
 
-    public class Save
+    public static class SaveSystem
     {
-    
+        private static string SaveRootPath => string.Intern(Application.dataPath);
+        private static string SaveFolder => string.Intern("save");
+        private static string Salt => string.Intern("2ijowfnmn");
+
+        public static void Save(SaveData saveData)
+        {
+            var json = saveData.ToJson();
+            var saveRootPath = string.Intern($"{SaveRootPath}/{SaveFolder}");
+            if (!System.IO.Directory.Exists(saveRootPath))
+            {
+                System.IO.Directory.CreateDirectory(saveRootPath);
+            }
+            var savePath = string.Intern($"{saveRootPath}/save");
+            System.IO.File.WriteAllLines(savePath, new[] {json, Hash128.Compute(json + Salt).ToString()});
+        }
+
+        public static T Load<T>() where T : SaveData
+        {
+            var saveRootPath = string.Intern($"{SaveRootPath}/{SaveFolder}");
+            var savePath = string.Intern($"{saveRootPath}/save");
+            var values = System.IO.File.ReadAllLines(savePath);
+            if (values[1] != Hash128.Compute(values[0] + Salt).ToString())
+            {
+                return default(T);
+            }
+            return JsonUtility.FromJson<T>(values[0]);
+        }
     }
 }
