50597be09a2cc7d014974bb75803050582b5ff3d
wanibananalab <wanideath@gmail.com>
2021-06-21 23:10:06 +0900

    雑に更新

---
+++ Assets/Scenes/001_Save/SampleSave.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test
+{
+    public class SampleSave : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+
+    [System.Serializable]
+    public class User : SaveData
+    {
+        public int id;
+        public int value;
+    }
+
+    public class SaveData
+    {
+        public string ToJson()
+        {
+            return JsonUtility.ToJson(this);
+        }
+    }
+
+    public class Save
+    {
+    
+    }
+}
